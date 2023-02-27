03fc3ae4e82c42442a488a1eb7716282b61bfbc2
shz998 <svesald@gmail.com>
2023-02-27 11:58:51 +0330

    new req

--- django_project/settings.py
+++ django_project/settings.py
@@ -39,16 +39,21 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     #local 
     'todos.apps.TodosConfig',
+    #3rd party
+    "rest_framework",
+    "coresheaders",
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'django_project.urls'
@@ -123,3 +128,18 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#new
+
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.AllowAny",
+    ],
+}
+
+CORES_ALLOWED_ORIGINS = (
+    "http://localhost:3000",
+    "http://localhost:3000",
+)
+
+CSRF_TRUSTED_ORIGINS = ["localhost:3000"]
